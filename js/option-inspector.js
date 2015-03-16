2314f588e1d5fbf8c3c278ad8584ddad6a27d554
Viktor Szépe <viktor@szepe.net>
2015-03-17 00:51:32 +0100

    for smaller screens

--- js/option-inspector.js
+++ js/option-inspector.js
@@ -7,14 +7,23 @@ jQuery(function ($) {
 
     ser_opts.each(function (){
         var header, dashicon,
+            viewport = { maxx: 1100, maxy: 760 },
             input = $(this),
             icon_html = '<span class="dashicons dashicons-editor-expand"></span>';
+
+        viewport.x = $(window).width() - 170;
+        if (viewport.x > viewport.maxx)
+            viewport.x = viewport.maxx;
+        viewport.y = $(window).height() - 72;
+        if (viewport.y > viewport.maxy)
+            viewport.y = viewport.maxy;
 
         dashicon = input.wrap('<a class="inspector"></div>').parent().append(icon_html);
         header = input.parent().parent().prev();
         $.merge(dashicon, header).click(function () {
             var name = input.attr('name');
-            tb_show(name, 'admin-ajax.php?action=o1_inspect_option&option_name=' + name + '&width=1100&height=760&_nonce=' + OPTIONINS.nonce);
+            tb_show(name, 'admin-ajax.php?action=o1_inspect_option&option_name=' + name +
+                '&width=' + viewport.x + '&height=' + viewport.y + '&_nonce=' + OPTIONINS.nonce);
         });
     });
 });
